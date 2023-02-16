594f4c3479b0e41c1647390fe9ea1e73d814fdf5
Simeone-Holanda <simeone.10s10@gmail.com>
2023-02-16 18:55:23 -0300

    [Adicionado]: Configurações do projeto como cores e fonts com o chakra-ui.

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import { Text } from "@chakra-ui/react";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Text color={'yellowButton'}> Hello world. </Text>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/styles/theme.js
@@ -0,0 +1,17 @@
+import { extendTheme } from '@chakra-ui/react'
+
+
+export const theme = extendTheme({
+    colors: {
+        yellowButton: '#F4C23B',
+        text: '#333333',
+        grayDivTable: '#E0E0E0',
+        softGray: '#828282',
+        container: '#69708C'
+    },
+    fonts: {
+        // heading: 'Poppins',
+        heading: 'Roboto',
+        body: 'Roboto'
+    }
+})

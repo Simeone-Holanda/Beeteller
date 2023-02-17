d540c02b8a0df72745b7f1ca6deaa81c95bc7710
Simeone-Holanda <simeone.10s10@gmail.com>
2023-02-17 18:28:31 -0300

    [Adicionado]: Bliblioteca dayjs e mais algumas cores ao thema do projeto.

--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -7,7 +7,8 @@ export const theme = extendTheme({
         text: '#333333',
         grayDivTable: '#E0E0E0',
         softGray: '#828282',
-        container: '#69708C'
+        cotationGray: '#BDBDBD',
+        container: '#ECF6FF'
     },
     fonts: {
         // heading: 'Poppins',

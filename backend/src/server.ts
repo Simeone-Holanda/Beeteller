4ea21a7595ff06116960ac2c5ab549e7474a4f76
Simeone-Holanda <simeone.10s10@gmail.com>
2023-02-20 03:59:43 -0300

    [Adicionado]: Novas bibliotecas e a sincronização com o banco de dados ao iniciar o backend.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,18 @@
 import http from 'http'
 import app from './app'
+import sequelize from './database'
+import 'dotenv/config'
 
-const port = process.env.PORT || 3000
-const server = http.createServer(app)
 
-console.log(`App starded in port ${port}!`)
-server.listen(port)
+sequelize.sync()
+    .then(() => {
+        const server = http.createServer(app)
+        const port = process.env.PORT || 3000
+        console.log(`App starded in port ${port}!`)
+        server.listen(port)
+    })
+    .catch(err => {
+        console.error('Unable to synchronize models:', err);
+    });
+
 
